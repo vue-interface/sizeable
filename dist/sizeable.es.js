bfed9c0da763381bc4f2bd623cc720683e6e4efe
Justin Kimbrell <jkimbrell@actengage.com>
2022-02-02 20:29:04 -0700

    pre-release commit

---
+++ dist/sizeable.es.js
@@ -0,0 +1,23 @@
+var Sizeable = {
+  props: {
+    size: String,
+    sizePrefix: {
+      type: String,
+      default() {
+        return this.$options.name;
+      }
+    }
+  },
+  computed: {
+    sizeableClassPrefix() {
+      return this.sizePrefix;
+    },
+    sizeableClass() {
+      if (!this.size || !this.sizeableClassPrefix) {
+        return "";
+      }
+      return `${this.sizeableClassPrefix}-${this.size}`;
+    }
+  }
+};
+export { Sizeable };
